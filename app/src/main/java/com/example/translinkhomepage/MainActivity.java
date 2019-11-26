4cf8d1bec74958497d0b6f73e11be0bd2f0011db
Daveywilson <wilson-d31@ulster.ac.uk>
2019-11-26 15:03:44 +0000

    Translink Logo

--- app/src/main/java/com/example/translinkhomepage/MainActivity.java
+++ app/src/main/java/com/example/translinkhomepage/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     Button firstButton;
@@ -14,14 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        final ImageView FirstImage = (ImageView) findViewById(R.id.FirstImage);
+        int imageResource = getResources().getIdentifier("@drawable/translink",null,this.getPackageName());
+        FirstImage.setImageResource(imageResource);
         firstButton = findViewById(R.id.BtnToSecond);
-        firstButton.setOnClickListener(new View.OnClickListener() {
+        firstButton.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, NextActivity.class);
                 startActivity(intent);
+
             }
         });
+
     }
 }
